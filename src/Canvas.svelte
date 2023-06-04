1515a69b9096842f3c51d30981dad1f88b82ccf6
Trevor <trevor@osrs.tools>
2023-06-05 07:34:06 +0900

    Scaffold inital interactive canvas workarea

--- src/Canvas.svelte
+++ src/Canvas.svelte
@@ -1,41 +1,37 @@
 <script lang=ts>
   import { canvasWidth, canvasHeight } from './stores/canvas'
+  import Cell from './Cell.svelte'
+    import { onMount } from 'svelte';
 
+  let workareaReady = false
+  let workareaWidth = 1
   let canvas: HTMLCanvasElement
+
+  $: workareaSize = Math.round(workareaWidth / 22)
+
+  onMount(() => {
+    // transition in nicely regardless of light/dark mode
+    workareaReady = true
+  })
 </script>
 
 <div class='absolute inset-0 flex flex-col justify-evenly items-center'>
   <!-- Auto scrolling, zoomed-in canvas preview. Follows cursor as it draws-->
-  <!-- todo: auto-set element width based on difference of current canvas height -->
-  <section class='max-h-full w-full'>
-    <div class='w-full'>
-      <canvas
-        class='mx-auto'
-        width=64
-        height=64
-      />
-    </div>
-    <div class='w-full'>
-      <canvas
-        class='mx-auto'
-        width=64
-        height=64
-      />
-    </div>
-    <div class='w-full'>
-      <canvas
-        class='mx-auto'
-        width=64
-        height=64
-      />
-    </div>
-
-    <div class='w-full'>
-      <canvas
-        class='mx-auto'
-        width=64
-        height=64
-      />
+  <section class='p-2 max-h-full w-full'>
+    <div
+      bind:clientWidth={workareaWidth}
+      class='workarea-bg w-full max-w-md mx-auto'
+      class:opacity-0={!workareaReady}
+      class:opacity-100={workareaReady}
+    >
+      <div
+        class='h-full workarea grid'
+        style={`--workarea-size:${workareaSize}`}
+      >
+        {#each new Array(workareaSize ** 2) as _idx}
+          <Cell />
+        {/each}
+      </div>
     </div>
   </section>
 
@@ -55,8 +51,36 @@
   </section>
 </div>
 
-<style>
+<style lang=postcss>
   canvas {
     outline: 0.1px solid;
+  }
+
+  @keyframes workarea-pan {
+    from {
+      background-position: 0% center;
+    }
+
+    to {
+      background-position: -200% center;
+    }
+  }
+
+  .workarea-bg {
+    aspect-ratio: 1 / 1;
+    transition: opacity 0.2s ease-in;
+    animation: workarea-pan 5s ease-in-out infinite;
+    background: linear-gradient(
+      to right,
+      rgb(var(--color-fg) / 0.5),
+      rgb(var(--color-bg) / 1),
+      rgb(var(--color-fg) / 0.5)
+    );
+    background-size: 200%;
+  }
+
+  .workarea {
+    grid-template-columns: repeat(var(--workarea-size), 1fr);
+    grid-template-rows: repeat(var(--workarea-size), 1fr);
   }
 </style>
---
+++ src/Cell.svelte
@@ -0,0 +1,30 @@
+<script lang=ts>
+  // export let onClick: () => void
+</script>
+
+<button
+  class='cell-button relative before:bg-bg before:absolute before:inset-[0.5px]'
+>
+  <span class=sr-only>Click to select cell #, #</span>
+</button>
+
+<style lang=postcss>
+  button {
+    &:before {
+      transition: background-color 0.1s;
+    }
+
+    &:hover,
+    &:focus {
+      &:before {
+        transition: background-color 0s;
+        background-color: rgb(var(--color-bg) / 1);
+        outline: 1.5px solid #6600FF; /* todo: standardize color */
+        outline-offset: -1px;
+        inset: 0px;
+        transition: box-shadow 0.125s;
+        box-shadow: inset 0 0 1.5px 1.5px rgb(var(--color-fg) / 1);
+      }
+    }
+  }
+</style>
